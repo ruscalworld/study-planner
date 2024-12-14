c08e25bf04acf1ca4aa7152ea07fa0e3e23fb097
Nikolay <60034902+ruscalworld@users.noreply.github.com>
2024-12-14 08:46:49 +0300

    Implement OpenID auth via Google (#5)
    * Implement OAuth via Google
    
    * go mod tidy
    
    * Update deps
    
    * Attempt to implement Google JWT auth flow

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/ruscalworld/study-planner
 go 1.21
 
 require (
-	github.com/bwmarrin/discordgo v0.28.1
+	github.com/coreos/go-oidc/v3 v3.11.0
 	github.com/go-sql-driver/mysql v1.8.1
 	github.com/gofiber/fiber/v2 v2.52.5
 	github.com/golang-jwt/jwt/v5 v5.2.1
@@ -13,12 +13,12 @@ require (
 )
 
 require (
+	cloud.google.com/go/compute/metadata v0.5.2 // indirect
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/andybalholm/brotli v1.1.1 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.5 // indirect
-	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/go-jose/go-jose/v4 v4.0.4 // indirect
 	github.com/google/uuid v1.6.0 // indirect
-	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/klauspost/compress v1.17.11 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
@@ -26,12 +26,9 @@ require (
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.57.0 // indirect
+	github.com/valyala/fasthttp v1.58.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
-	golang.org/x/crypto v0.29.0 // indirect
-	golang.org/x/net v0.31.0 // indirect
-	golang.org/x/sys v0.27.0 // indirect
-	google.golang.org/appengine v1.6.8 // indirect
-	google.golang.org/protobuf v1.35.2 // indirect
+	golang.org/x/crypto v0.31.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
 )
--- internal/auth/platform/oauth.go
+++ internal/auth/platform/oauth.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/ruscalworld/study-planner/internal/auth"
 
+	"github.com/coreos/go-oidc/v3/oidc"
 	"golang.org/x/oauth2"
 )
 
@@ -14,7 +15,8 @@ type UserInfoSupplier interface {
 }
 
 type CodeRequest struct {
-	Code string `json:"code"`
+	Code    string `json:"code"`
+	IdToken string `json:"id_token"`
 }
 
 type AuthenticationConfig struct {
@@ -23,11 +25,21 @@ type AuthenticationConfig struct {
 
 type OAuthPlatform struct {
 	config   *oauth2.Config
-	supplier UserInfoSupplier
+	provider *oidc.Provider
+	verifier *oidc.IDTokenVerifier
 }
 
-func NewOAuthPlatform(config *oauth2.Config, supplier UserInfoSupplier) *OAuthPlatform {
-	return &OAuthPlatform{config: config, supplier: supplier}
+func NewOAuthPlatform(config *oauth2.Config, oidIssuer string) (*OAuthPlatform, error) {
+	provider, err := oidc.NewProvider(context.Background(), oidIssuer)
+	if err != nil {
+		return nil, err
+	}
+
+	return &OAuthPlatform{
+		config:   config,
+		provider: provider,
+		verifier: provider.Verifier(&oidc.Config{ClientID: config.ClientID}),
+	}, nil
 }
 
 func (p *OAuthPlatform) GetAuthenticationConfig(_ context.Context) (*AuthenticationConfig, error) {
@@ -37,10 +49,34 @@ func (p *OAuthPlatform) GetAuthenticationConfig(_ context.Context) (*Authenticat
 }
 
 func (p *OAuthPlatform) Authenticate(ctx context.Context, request *CodeRequest) (*auth.UserInfo, error) {
-	token, err := p.config.Exchange(ctx, request.Code)
-	if err != nil {
-		return nil, fmt.Errorf("code exchange: %s", err)
+	if request.IdToken == "" {
+		token, err := p.config.Exchange(ctx, request.Code)
+		if err != nil {
+			return nil, fmt.Errorf("code exchange: %s", err)
+		}
+
+		rawIdToken, ok := token.Extra("id_token").(string)
+		if !ok {
+			return nil, fmt.Errorf("id_token is missing or is not a string")
+		}
+
+		request.IdToken = rawIdToken
 	}
 
-	return p.supplier.GetUserInfo(token)
+	idToken, err := p.verifier.Verify(ctx, request.IdToken)
+	if err != nil {
+		return nil, fmt.Errorf("id token verification: %s", err)
+	}
+
+	var u GoogleUser
+	if err := idToken.Claims(&u); err != nil {
+		return nil, fmt.Errorf("parsing claims: %s", err)
+	}
+
+	return &auth.UserInfo{
+		ExternalID: u.ID,
+		Platform:   "google",
+		Name:       u.Name,
+		AvatarURL:  u.Picture,
+	}, nil
 }
